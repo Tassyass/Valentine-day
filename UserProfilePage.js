e056a9f0b0706b84952cf0480fc2bf05ada1c32c
Tassyass <tasniim@DESKTOP-5THH7BR>
2024-02-06 09:42:04 +0300

    initial commit

---
+++ UserProfilePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function UserProfilePage() {
+  return (
+    <div>
+      <h1>User Profile</h1>
+      {/* Display user's gift lists and gifts */}
+    </div>
+  );
+}
+
+export default UserProfilePage;
